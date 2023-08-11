77233b70995661030ca4e0bf457cf0d9334c4f7d
vssmatheus <vssmatheus@gmail.com>
2023-08-11 00:11:02 -0300

    ajustes no arduino

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
  const api = axios.create({
-    baseURL: 'http://192.168.100.30',
+   // ip local do arduino | verificar no server monitor do arduino
+    baseURL: 'http://192.168.1.13/',
  })
 
  export default api;
